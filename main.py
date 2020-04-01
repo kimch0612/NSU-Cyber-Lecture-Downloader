5762204dfdf98865a34098593c054edb732a329e
Changhwan Kim <chals@nsu.ac.kr>
2020-04-02 02:00:47 +0900

    문구 수정

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess
 
-a = input()
-b = input()
-subprocess.call ('ffmpeg -i http://%s/playlist.m3u8 -acodec copy -absf aac_adtstoasc %s.mp4'% (a, b), shell=True)
+url = input("RTMP 형식의 URL을 입력하여 주세요 : ")
+name = input("저장할 이름을 입력해 주세요 : ")
+subprocess.call ('ffmpeg -i http://%s/playlist.m3u8 -acodec copy -absf aac_adtstoasc %s.mp4'% (url, name), shell=True)
