18e6a8eeb02d5b1a414eae2795405c9f8bf8ae61
Changhwan Kim <chals@nsu.ac.kr>
2020-04-07 12:57:08 +0900

    안내 문구 수정

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import subprocess
 
 name = []
 file = []
-i = 0
 
 down = input("다운로드 받을 영상의 총 개수를 입력하여 주세요 (예: 3) : ")
 down1 = int(down)
-gangjoa = input("다운로드할 강좌의 '강좌 코드'를 입력해 주세요 : ")
-gangyee = input("다운로드할 강좌의 '강의 코드'를 입력해 주세요 : ")
+gangjoa = input("다운로드할 강좌의 '강좌 코드'를 입력해 주세요 (5자리 숫자로 구성 됨) : ")
+gangyee = input("다운로드할 강좌의 '강의 코드'를 입력해 주세요 (5자리 숫자로 구성 됨) : ")
 
 for a in range(1, down1+1):
-    g1 = input("%s번째로 다운로드할 강좌의 '영상 이름'을 입력해 주세요 : " % (a))
+    print("%s번째로 다운로드할 강좌의 '영상 이름'을 입력해 주세요" % (a))
+    g1 = input("입력 예시 : xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
     name.append(g1)
-    g2 = input("%s번째로 다운로드할 강좌의 파일 이름을 입력해 주세요 : " % (a))
+    g2 = input("%s번째로 다운로드할 강좌의 파일 이름을 입력해 주세요 (예 : 1st) : " % (a))
     file.append(g2)
     a += 1
 
