04d43d12a28c174fb7ff93430cee25531977c8d9
Changhwan Kim <chals@nsu.ac.kr>
2020-04-13 13:37:27 +0900

    파일을 다운로드 받을 때 강의 코드를 이름으로 한 폴더를 만들고 그 안에 다운로드가 되도록 코드 수정

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 name = []
 file = []
@@ -15,6 +16,13 @@ for a in range(1, down1+1):
     g2 = input("%s번째로 다운로드할 강좌의 파일 이름을 입력해 주세요 (예 : 1st) : " % (a))
     file.append(g2)
     a += 1
+
+try:
+    os.mkdir("%s" % (gangyee))
+except:
+    pass
+
+os.chdir(gangyee)
 
 for i in range(0, down1):
     subprocess.call ('ffmpeg -i http://nsu.vod.cdn.cloudn.co.kr/nsu/_definst_/nsucdn_nsu/lms_nsu/course_vod/%s/%s/%s.mp4/playlist.m3u8 -preset veryfast -acodec copy -b:v 6000k %s.mp4'% (gangjoa, gangyee, name[i], file[i]), shell=True)
