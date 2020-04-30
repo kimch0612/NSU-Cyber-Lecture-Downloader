da8676e8b7166f794b1e048c10849970985acbe8
Changhwan Kim <chals@nsu.ac.kr>
2020-04-30 16:20:03 +0900

    다운로드시 파일 이름을 자동으로 설정해주도록 코드 수정

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ for a in range(1, down1+1):
     print("%s번째로 다운로드할 강좌의 '영상 이름'을 입력해 주세요" % (a))
     g1 = input("입력 예시 : xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.mp4 : ")
     name.append(g1)
-    g2 = input("%s번째로 다운로드할 강좌의 파일 이름을 입력해 주세요 (예 : 1st) : " % (a))
-    file.append(g2)
     a += 1
 
 try:
@@ -25,7 +23,7 @@ except:
 os.chdir(gangjoa)
 
 for i in range(0, down1):
-    subprocess.call ('ffmpeg -i http://nsu.vod.cdn.cloudn.co.kr/nsu/_definst_/nsucdn_nsu/lms_nsu/course_vod/%s/%s/%s/playlist.m3u8 -preset veryfast -acodec copy -b:v 6000k %s.mp4'% (gangjoa, gangyee, name[i], file[i]), shell=True)
+    subprocess.call ('ffmpeg -i http://nsu.vod.cdn.cloudn.co.kr/nsu/_definst_/nsucdn_nsu/lms_nsu/course_vod/%s/%s/%s/playlist.m3u8 -preset veryfast -acodec copy -b:v 6000k %s.mp4'% (gangjoa, gangyee, name[i], i+1), shell=True)
     i += 1
 
 print("Done!")
